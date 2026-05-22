71ce1887b902bfbaf3f7f2ef7af77aa84a54017d
Tulio028 <marcotuliogxs@gmail.com>
2026-05-22 18:54:47 -0300

    Testando java

---
+++ App.java
@@ -0,0 +1,9 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        char letra;
+        for (int i = 0; i < 26; i++) {
+            letra = (char) (65+i);
+            System.out.println(letra);
+        }
+    }
+}
